82864c8629fad0fcf8c5c9978259cc1657cae5d0
DoctorK4 <doctor.k1994@gmail.com>
2023-01-17 16:08:34 +0900

    delay 함수 생성

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -1,24 +1,105 @@
-function xhrData (method, url){
+export function xhrData ({
+  url = '',
+  method = 'GET', 
+  body = null,
+  onSuccess = null,
+  onFail = null,
+  headers = {
+  'Content=Type':'application/json',
+  'Access-Control-Allow-Origin':'*',
+  }
+}= {}){
+  
+  // const { url, method, body} = option;
   const xhr = new XMLHttpRequest();
-  xhr.open(method, url)
+
+  // 비동기 통신 오픈
+  xhr.open(method, url);
+  
+  // headers를 배열로 반환해주고, setRequestHeader 내장 함수 사용 
+  // Object.entries(headers).forEach(([key, value])=>{
+  //   xhr.setRequestHeader(key, value);
+  // })
+
+  
+
   xhr.addEventListener('readystatechange', ()=>{
-    if(xhr.status >= 200 && xhr.status < 400){
-      if(xhr.readyState === 4) {
+    const { status, readyState, response } = xhr; // 객체 구조 분해 할당
+    if(status >= 200 && status < 400){
+      if(readyState === 4) {
         console.log('통신 성공');
+        onSuccess(JSON.parse(response));
       } 
     } else {
-        console.log('통신 실패');
+        // console.error('통신 실패');
+        onFail('통신 실패');
       }
   })
 
   // 서버에 요청
-  xhr.send();
+  xhr.send(JSON.stringify(body));
 
 }
 
 
-// 비동기 통신 오픈
-xhrData('GET','https://jsonplaceholder.typicode.com/users' )
+// xhrData({
+//   url:'https://jsonplaceholder.typicode.com/users',
+//   onSuccess: (result) => {
+//     console.log(result);
+//   },
+//   onFail:(err)=>{
+//     console.error(err);
+//   }
+// })
+
+xhrData.get = (url, onSuccess, onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+
+xhrData.post = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method:'POST',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+xhrData.put = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method:'PUT',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+xhrData.delete = (url, onSuccess, onFail) => {
+  xhrData({
+    method:'DELETE',
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+
+
+// xhrData.delete(
+//   'https://jsonplaceholder.typicode.com/users/1',
+//   (result)=>{
+//     console.log(result);
+//   },
+//   (err)=>{
+//     console.log(err);
+//   }
+// )
+
 
 /* readyState
   0: uninitalized : 초기화
--- client/main.js
+++ client/main.js
@@ -1,1 +1,11 @@
-import { } from "./lib/index.js";
+import { xhrData, insertLast } from "./lib/index.js";
+
+// xhrData.get(
+//   'https://jsonplaceholder.typicode.com/users/1',
+//   (res)=>{
+//     insertLast('body',JSON.stringify(res));
+//   },
+//   (err)=>{
+//     insertLast('body',err);
+//   }
+// )
